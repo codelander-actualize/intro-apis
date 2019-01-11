31b0e9de90061414a808b6f23d6cafc32727b7a8
dani <dani.zaghian@gmail.com>
2019-01-11 14:10:26 -0800

    adds dictionary app solutions

--- api_app.rb
+++ api_app.rb
@@ -7,5 +7,25 @@ word = gets.chomp
 
 response = HTTP.get("https://api.wordnik.com/v4/word.json/#{word}/definitions?limit=10&includeRelated=false&useCanonical=false&includeTags=false&api_key=ac6099e63826b8650f05e22c4cc08baa2f21668e3f16176fd")
 
-puts JSON.pretty_generate(response.parse)
+top_example = HTTP.get("https://api.wordnik.com/v4/word.json/#{word}/topExample?useCanonical=false&api_key=ac6099e63826b8650f05e22c4cc08baa2f21668e3f16176fd")
+
+pronunciation = HTTP.get("https://api.wordnik.com/v4/word.json/#{word}/pronunciations?useCanonical=false&limit=5&api_key=ac6099e63826b8650f05e22c4cc08baa2f21668e3f16176fd")
+
+audio = HTTP.get("https://api.wordnik.com/v4/word.json/#{word}/audio?useCanonical=false&limit=1&api_key=ac6099e63826b8650f05e22c4cc08baa2f21668e3f16176fd")
+
+# puts JSON.pretty_generate(response.parse[1]["text"])
+
+puts "DEFINITIONS: "
+response.parse.each do |definition|
+	puts JSON.pretty_generate("#{definition["text"]}")
+end
+
+puts "TOP EXAMPLE: "
+puts JSON.pretty_generate(top_example.parse["text"])
+
+puts "PRONUNCIATION: "
+puts JSON.pretty_generate(pronunciation.parse[0]["raw"])
+
+puts "AUDIO: "
+`open #{audio.parse[0]["fileUrl"]}`
 
